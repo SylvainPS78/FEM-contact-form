177ebe7b2e2ab5ea593ae50c653dc136b5737c88
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-03-25 12:41:56 +0700

    FEAT Add JS logic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,84 @@
+const submitButton = document.getElementById("submit-button");
+const radioElements = document.querySelectorAll(".radioElement");
+const confirmationMessage = document.getElementById("confirmationMessage");
+let isError = false;
 
-//  const nameInput = document.querySelector('[name="name"]');
+const initAllErrors = () => {
+    const allInputs = document.querySelectorAll("input");
+    const textArea = document.querySelector("textarea");
+    const errorMessages = document.querySelectorAll(".error-message");
+    isError = false;
 
-// nameInput.addEventListener('invalid', () => {
-//   nameInput.setCustomValidity('Please enter your name.'); });
+    errorMessages.forEach(message => {
+        message.style.visibility = "hidden";
+    })
+    allInputs.forEach(input => {
+        input.style.border = "1px solid var(--text-grey)";
+    })
+    textArea.style.border = "1px solid var(--text-grey)";
+    radioElements.forEach(radioInput => {
+        radioInput.style.border = "1px solid var(--text-grey)"});
+}
 
-// gérer le aria-describedby
+const errorMessageDisplay = () => {
+    const textEmailInput = document.querySelectorAll('input[type="text"], input[type="email"]');
+    const queryTypeSelected = document.querySelector("input[name='queryType']:checked");
+    const consentBoxSelected = document.querySelector("input[name='consent']:checked");
+    const textArea = document.querySelector("textarea");
+
+    textEmailInput.forEach(input => {
+        if (!input.value) {
+            let inputName = input.name;
+            document.getElementById(`${inputName}-error`).textContent = `This field is required`;
+            document.getElementById(`${inputName}-error`).style.visibility = "visible";
+            input.style.border = "1px solid var(--color-error)";
+            isError = true;
+        }
+        else if (!input.checkValidity()) {
+            let inputName = input.name;
+            document.getElementById(`${inputName}-error`).textContent = `Please enter a valid input`;
+            document.getElementById(`${inputName}-error`).style.visibility = "visible";
+            input.style.border = "1px solid var(--color-error)";
+            isError = true;
+        }
+    });
+
+    if (!queryTypeSelected) {
+        document.getElementById(`queryType-error`).style.visibility = "visible";
+        radioElements.forEach((radioInput) => {
+            radioInput.style.border = "1px solid var(--color-error)";
+            isError = true;
+        });
+    }
+
+    if (!consentBoxSelected) {
+        document.getElementById(`consent-error`).textContent = "You must consent to be contacted"; // Ajout du message d'erreur spécifique
+        document.getElementById(`consent-error`).style.visibility = "visible";
+        document.querySelector("#consent").style.border = "1px solid var(--color-error)";
+        isError = true;
+    }
+
+    if (!textArea.value) {
+        document.getElementById("message-error").textContent = "This field is required";
+        document.getElementById("message-error").style.visibility = "visible";
+        textArea.style.border = "1px solid var(--color-error)";
+        isError = true;
+    }
+}
+
+const displayConfirmationMessage = () => {
+    if (!isError) {
+        confirmationMessage.style.display = "grid";
+    }
+}
+
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    initAllErrors();
+    errorMessageDisplay();
+    displayConfirmationMessage();
+});
+
+
+
+
